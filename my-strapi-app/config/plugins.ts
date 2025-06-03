7546cec36294ae99feee194db593c9493cf6b2a6
fazalhaq661 <fazalhaq6870542@gmail.com>
2025-06-03 16:38:26 +0430

    User Authentication Feature

--- my-strapi-app/config/plugins.ts
+++ my-strapi-app/config/plugins.ts
@@ -1,14 +1,35 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   email: {
     config: {
-      provider: 'sendgrid',
+      provider: "sendgrid",
       providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
+        apiKey: env("SENDGRID_API_KEY"), // Required
       },
       settings: {
-        defaultFrom: 'fazalhaq6870542@gmail.com',
-        defaultReplyTo: 'fazalhaq6870542@gmail.com',
+        defaultFrom: env("SENDGRID_EMAIL"),
+        defaultReplyTo: env("SENDGRID_EMAIL"),
       },
     },
   },
 });
+
+// module.exports = {
+//   email: {
+//     config: {
+//       provider: 'nodemailer',
+//       providerOptions: {
+//         host: 'sandbox.smtp.mailtrap.io',
+//         port: 2525,
+//         auth: {
+//           user: '4d5f1cb6c9052f',
+//           pass: '2b5936f46c935c',
+//         },
+//       },
+//       settings: {
+//         defaultFrom: 'noreply@example.com',
+//         defaultReplyTo: 'support@example.com',
+//       },
+//     },
+//   },
+// };
+
--- my-strapi-app/types/generated/contentTypes.d.ts
+++ my-strapi-app/types/generated/contentTypes.d.ts
@@ -551,6 +551,34 @@ export interface ApiLandingPageLandingPage extends Struct.SingleTypeSchema {
   };
 }
 
+export interface ApiProductProduct extends Struct.CollectionTypeSchema {
+  collectionName: 'products';
+  info: {
+    displayName: 'Product';
+    pluralName: 'products';
+    singularName: 'product';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::product.product'
+    > &
+      Schema.Attribute.Private;
+    name: Schema.Attribute.String;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -1065,6 +1093,7 @@ declare module '@strapi/strapi' {
       'api::email-service.email-service': ApiEmailServiceEmailService;
       'api::global.global': ApiGlobalGlobal;
       'api::landing-page.landing-page': ApiLandingPageLandingPage;
+      'api::product.product': ApiProductProduct;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
